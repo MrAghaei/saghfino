61aede1281d00ed0843d828b0859e380a186d12e
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-02-21 10:56:57 +0330

    [#3-ADD] Header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeNewUserComponent } from './HomeNewUserPage/components/home-new-user/home-new-user.component'
+import { HeaderComponent } from './components/header/header.component'
+import { NgOptimizedImage } from '@angular/common'
 
 @NgModule({
-  declarations: [AppComponent, HomeNewUserComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, HomeNewUserComponent, HeaderComponent],
+  imports: [BrowserModule, AppRoutingModule, NgOptimizedImage],
   providers: [],
   bootstrap: [AppComponent],
 })
