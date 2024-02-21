6d482f11bddeea20efdd8a2b0a453de64b5205c6
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-02-21 11:57:42 +0330

    [#3-UPDATE] header component
    generate navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeNewUserComponent } from './HomeNewUserPage/components/home-new-user/home-new-user.component'
 import { HeaderComponent } from './components/header/header.component'
-import { NgOptimizedImage } from '@angular/common'
+import { NgOptimizedImage } from '@angular/common';
+import { NavbarComponent } from './components/navbar/navbar.component'
 
 @NgModule({
-  declarations: [AppComponent, HomeNewUserComponent, HeaderComponent],
+  declarations: [AppComponent, HomeNewUserComponent, HeaderComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, NgOptimizedImage],
   providers: [],
   bootstrap: [AppComponent],
