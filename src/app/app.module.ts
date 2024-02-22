8bf98b8f7696e69d15074e95f768fa53f1692219
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-02-22 09:57:54 +0330

    [#5-ADD] Searchbar component (#6)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,19 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HomeNewUserComponent } from './HomeNewUserPage/components/home-new-user/home-new-user.component'
 import { HeaderComponent } from './components/header/header.component'
-import { NgOptimizedImage } from '@angular/common';
+import { NgOptimizedImage } from '@angular/common'
 import { NavbarComponent } from './components/navbar/navbar.component'
+import { SearchbarComponent } from './components/searchbar/searchbar.component'
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
+
+import { MatInputModule } from '@angular/material/input'
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatIconModule } from '@angular/material/icon'
 
 @NgModule({
-  declarations: [AppComponent, HomeNewUserComponent, HeaderComponent, NavbarComponent],
-  imports: [BrowserModule, AppRoutingModule, NgOptimizedImage],
-  providers: [],
+  declarations: [AppComponent, HomeNewUserComponent, HeaderComponent, NavbarComponent, SearchbarComponent],
+  imports: [BrowserModule, AppRoutingModule, NgOptimizedImage, MatInputModule, MatFormFieldModule, MatIconModule],
+  providers: [provideAnimationsAsync()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
