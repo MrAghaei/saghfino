bdedd48d13079920b4785554edeec9c117fce63f
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-03-03 11:03:25 +0330

    [#23-UPDATE] make search page heading dynamic (#28)

--- src/app/components/search-page/top-real-estate/top-real-estate.component.ts
+++ src/app/components/search-page/top-real-estate/top-real-estate.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { map } from 'rxjs'
+import { ActivatedRoute } from '@angular/router'
 
 @Component({
   selector: 'app-top-real-estate',
   templateUrl: './top-real-estate.component.html',
-  styleUrl: './top-real-estate.component.scss'
+  styleUrl: './top-real-estate.component.scss',
 })
 export class TopRealEstateComponent {
+  user$ = this.route.queryParams.pipe(map((q) => q['city']))
 
+  constructor(private route: ActivatedRoute) {}
 }
