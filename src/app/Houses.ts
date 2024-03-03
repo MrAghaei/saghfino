02e031edd75254524230d7ef33f868c2abf5e2b4
Mohamad Aghaei <65438728+MrAghaei@users.noreply.github.com>
2024-03-03 10:49:48 +0330

    [#22-ADD] house model & data (#27)

---
+++ src/app/Houses.ts
@@ -0,0 +1,12 @@
+import { housesModel } from './housesModel'
+
+export const houses: housesModel[] = [
+  {
+    image: 'house1',
+    hoursPosted: '2',
+    area: '80',
+    address: 'رشت، گلسار، خیابان ۹۸',
+    rahnPrice: '500',
+    ejarePrice: '10',
+  },
+]
--- src/app/components/search-page/houses/houses.component.ts
+++ src/app/components/search-page/houses/houses.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
+import { houses } from '../../../Houses'
+import { housesModel } from '../../../housesModel'
 
 @Component({
   selector: 'app-houses',
   templateUrl: './houses.component.html',
-  styleUrl: './houses.component.scss'
+  styleUrl: './houses.component.scss',
 })
 export class HousesComponent {
-
+  housesData: housesModel[] = houses
 }
