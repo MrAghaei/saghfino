f589a045430b23b99857ea90acdad606ce77b058
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-03-03 10:49:26 +0330

    [#22-ADD] house model & data

--- src/app/components/search-page/houses-card/houses-card.component.ts
+++ src/app/components/search-page/houses-card/houses-card.component.ts
@@ -1,15 +1,11 @@
 import { Component, Input } from '@angular/core'
+import { housesModel } from '../../../housesModel'
 
 @Component({
   selector: 'app-houses-card',
   templateUrl: './houses-card.component.html',
-  styleUrl: './houses-card.component.scss',
+  styleUrls: ['./houses-card.component.scss'], // Corrected property name
 })
 export class HousesCardComponent {
-  @Input() image!: string
-  @Input() hoursPosted!: string
-  @Input() area!: string
-  @Input() address!: string
-  @Input() rahnPrice!: string
-  @Input() ejarePrice!: string
+  @Input() housesCardInputModel!: housesModel
 }
---
+++ src/app/housesModel.ts
@@ -0,0 +1,8 @@
+export interface housesModel {
+  image: string
+  hoursPosted: string
+  area: string
+  address: string
+  rahnPrice: string
+  ejarePrice: string
+}
