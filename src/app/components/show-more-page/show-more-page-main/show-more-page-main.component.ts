27ac327bf18fe6eb504a00418b4144d182a0ca3e
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-03-05 11:09:49 +0330

    [#29-UPDATE] showMorePage

---
+++ src/app/components/show-more-page/show-more-page-main/show-more-page-main.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-show-more-page-main',
+  templateUrl: './show-more-page-main.component.html',
+  styleUrl: './show-more-page-main.component.scss'
+})
+export class ShowMorePageMainComponent {
+
+}
