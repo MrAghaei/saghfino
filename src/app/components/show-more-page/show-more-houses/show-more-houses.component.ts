41974aee2ebd5801d8c919559f2e181b7ce19e5a
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-03-05 11:44:32 +0330

    [#29-ADD] routing to showmore

--- src/app/components/show-more-page/show-more-houses/show-more-houses.component.ts
+++ src/app/components/show-more-page/show-more-houses/show-more-houses.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core'
+import { housesModel } from '../../../housesModel'
 
 @Component({
   selector: 'app-show-more-houses',
   templateUrl: './show-more-houses.component.html',
-  styleUrl: './show-more-houses.component.scss'
+  styleUrl: './show-more-houses.component.scss',
 })
 export class ShowMoreHousesComponent {
-
+  @Input() housesData!: housesModel[]
+  @Input() cityName!: string
 }
