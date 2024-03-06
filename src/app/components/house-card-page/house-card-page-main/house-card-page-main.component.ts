ae3a5b73ccf446f22b68b06ce3014b2c7e6cf35f
Mohamad Aghaei <mmdrezaaghaei@gmail.com>
2024-03-06 09:48:11 +0330

    [#30-ADD] getHouseById in getHouse service

--- src/app/components/house-card-page/house-card-page-main/house-card-page-main.component.ts
+++ src/app/components/house-card-page/house-card-page-main/house-card-page-main.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core'
 import { Subscription } from 'rxjs'
 import { ActivatedRoute } from '@angular/router'
+import { housesModel } from '../../../housesModel'
+import { GetHousesService } from '../../../services/get-houses.service'
 
 @Component({
   selector: 'app-house-card-page-main',
@@ -9,16 +11,25 @@ import { ActivatedRoute } from '@angular/router'
 })
 export class HouseCardPageMainComponent {
   private queryParamsSubscription: Subscription | undefined
+  house!: housesModel
   houseId!: string
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private getHouses: GetHousesService,
+  ) {}
   ngOnInit() {
     this.queryParamsSubscription = this.route.queryParams.subscribe((params) => {
       this.houseId = params['houseId'] || ''
+      this.fetchHouseById()
+      console.log(this.house)
     })
   }
   ngOnDestroy() {
     if (this.queryParamsSubscription) {
       this.queryParamsSubscription.unsubscribe()
     }
+  }
+  fetchHouseById(): void {
+    this.house = this.getHouses.getHouseById(this.houseId)
   }
 }
